dc0e963b1e8c0a89ec172cc0d0a8f26455f7b4e3
nzamaltkhzyn-cmyk <nzamaltkhzyn@gmail.com>
2025-08-26 07:46:39 -0700

    Delete supabase-client.js

--- supabase-client.js
+++
@@ -1,5 +0,0 @@
-// supabase-client.js
-const SUPABASE_URL = 'https://inexkigotfyhsprhbqyn.supabase.co'; // استبدل هذا برابط مشروعك
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZXhraWdvdGZ5aHNwcmhicXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNTc4NDcsImV4cCI6MjA3MTYzMzg0N30.nkDBDL-BFHaTJAUdxzXrPqOzpWonDyr2jkKO2S_UwbU'; // استبدل هذا بمفتاح مشروعك
-
-supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
